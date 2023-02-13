9df7a4d779a63ce63264d825d7b52cd305939769
gabe-g-cmd <117952984+gabeStuk@users.noreply.github.com>
2023-02-13 17:05:09 -0500

    uncommented createversionfile

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -4,17 +4,16 @@ package frc.robot;
  * Automatically generated file containing build version information.
  */
 public final class BuildConstants {
-    public static final String MAVEN_GROUP = "";
-    public static final String MAVEN_NAME = "2023-ChargedUp";
-    public static final String VERSION = "unspecified";
-    public static final int GIT_REVISION = 4;
-    public static final String GIT_SHA = "6e6ed0887afbb9d45b7a48b46768da0231094689";
-    public static final String GIT_DATE = "2023-02-08 17:56:45 EST";
-    public static final String GIT_BRANCH = "drive_testing";
-    public static final String BUILD_DATE = "2023-02-09 19:34:37 EST";
-    public static final long BUILD_UNIX_TIME = 1675989277131L;
-    public static final int DIRTY = 1;
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "2023-ChargedUp";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = 20;
+  public static final String GIT_SHA = "a2c3d699d1539cf790f683080a8e82ba495ce322";
+  public static final String GIT_DATE = "2023-02-11 15:14:27 EST";
+  public static final String GIT_BRANCH = "practice_chassis_with_arms";
+  public static final String BUILD_DATE = "2023-02-13 16:32:27 EST";
+  public static final long BUILD_UNIX_TIME = 1676323947891L;
+  public static final int DIRTY = 1;
 
-    private BuildConstants() {
-    }
+  private BuildConstants(){}
 }
