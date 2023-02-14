ceee7ddd788e1d1995cafbec4183f992e635a751
gabe-g-cmd <117952984+gabeStuk@users.noreply.github.com>
2023-02-13 19:33:41 -0500

    Other changes (Ifrgot to stage)

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2023-ChargedUp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 20;
-  public static final String GIT_SHA = "a2c3d699d1539cf790f683080a8e82ba495ce322";
-  public static final String GIT_DATE = "2023-02-11 15:14:27 EST";
+  public static final int GIT_REVISION = 22;
+  public static final String GIT_SHA = "9df7a4d779a63ce63264d825d7b52cd305939769";
+  public static final String GIT_DATE = "2023-02-13 17:05:09 EST";
   public static final String GIT_BRANCH = "practice_chassis_with_arms";
-  public static final String BUILD_DATE = "2023-02-13 16:32:27 EST";
-  public static final long BUILD_UNIX_TIME = 1676323947891L;
+  public static final String BUILD_DATE = "2023-02-13 19:04:17 EST";
+  public static final long BUILD_UNIX_TIME = 1676333057292L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
--- src/main/java/frc/robot/commands/RunArmsToPosition.java
+++ src/main/java/frc/robot/commands/RunArmsToPosition.java
@@ -16,6 +16,10 @@ import frc.robot.subsystems.arms.UpperArm;
 // information, see:
 // https://docs.wpilib.org/en/stable/docs/software/commandbased/convenience-features.html
 public class RunArmsToPosition extends SequentialCommandGroup {
+    private double maxVel=7000;
+    private double maxAccel=14000;
+    private UpperArm m_upperarm=UpperArm.getInstance();
+    private LowerArm m_lowerarm=LowerArm.getInstance();
     /** Creates a new RunArmsToPosition. */
     public RunArmsToPosition(Arm.ArmPositions targetPositions, LowerArm lowerArm, UpperArm upperArm) {
         // Add your commands in the addCommands() call, e.g.
@@ -27,16 +31,16 @@ public class RunArmsToPosition extends SequentialCommandGroup {
                         // Begins extending lower arm,
                         // Then waits a period based on the distance needed to travel
                         // and then begins extending the upper arm.
-                        new RunArm(targetPositions.lowerPosition, lowerArm) 
+                        new TrapezoidRunArm(maxVel,maxAccel,m_lowerarm.getEncoderPosition(),targetPositions.lowerPosition,lowerArm) 
                                 .alongWith(new SuppliedWaitCommand(() -> lowerArm.getDistanceToTravel() / Constants.ArmConstants.EXTEND_COEFFICIENT)
-                                    .andThen(new RunArm(targetPositions.upperPosition, upperArm))),
+                                    .andThen(new TrapezoidRunArm(maxVel, maxAccel, m_upperarm.getEncoderPosition(), targetPositions.upperPosition, upperArm))),
                         // RETRACTING COMMAND
                         // Begins retracting upper arm,
                         // Then waits a period based on the distance needed to travel
                         // and then begins retracting the lower arm.
-                        new RunArm(targetPositions.upperPosition, upperArm)
+                        new TrapezoidRunArm(maxVel,maxAccel,m_lowerarm.getEncoderPosition(),targetPositions.lowerPosition,lowerArm)
                                 .alongWith(new SuppliedWaitCommand(() -> upperArm.getDistanceToTravel() / Constants.ArmConstants.RETRACT_COEFFICIENT)
-                                    .andThen(new RunArm(targetPositions.lowerPosition, lowerArm))),
+                                    .andThen(new TrapezoidRunArm(maxVel, maxAccel, m_upperarm.getEncoderPosition(), targetPositions.upperPosition, upperArm))),
                         () -> targetPositions.upperPosition > upperArm.getEncoderPosition()));
     }
 }
