cff95aa4e450b4148ad90af41af3541a82ca3554
Gabe <gabe.seq@gmail.com>
2023-02-25 11:16:06 -0500

    update vendordeps

--- src/main/java/frc/robot/BuildConstants.java
+++ src/main/java/frc/robot/BuildConstants.java
@@ -7,12 +7,12 @@ public final class BuildConstants {
   public static final String MAVEN_GROUP = "";
   public static final String MAVEN_NAME = "2023-ChargedUp";
   public static final String VERSION = "unspecified";
-  public static final int GIT_REVISION = 52;
-  public static final String GIT_SHA = "ed50c8bcb2cf30da0075464da6413188e1b9cf17";
-  public static final String GIT_DATE = "2023-02-23 17:24:08 EST";
-  public static final String GIT_BRANCH = "develop";
-  public static final String BUILD_DATE = "2023-02-23 17:35:47 EST";
-  public static final long BUILD_UNIX_TIME = 1677191747117L;
+  public static final int GIT_REVISION = 56;
+  public static final String GIT_SHA = "7cc7f6792af8d58d09ecfac103fae99104628564";
+  public static final String GIT_DATE = "2023-02-25 10:58:03 EST";
+  public static final String GIT_BRANCH = "composite-motion";
+  public static final String BUILD_DATE = "2023-02-25 11:14:58 EST";
+  public static final long BUILD_UNIX_TIME = 1677341698757L;
   public static final int DIRTY = 1;
 
   private BuildConstants(){}
