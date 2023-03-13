f2e80b3e1471a2c9e0b46e8a4941a692b1ed019a
gabe-g-cmd <117952984+gabeStuk@users.noreply.github.com>
2023-03-12 20:37:27 -0400

    Made commands to maybe fix kickstand

---
+++ src/main/java/frc/robot/commands/kickstand/AcitvateKickstand.java
@@ -0,0 +1,31 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands.kickstand;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.kickstand.Kickstand;
+
+public class AcitvateKickstand extends CommandBase {
+    private Kickstand m_kickstand;
+
+    /** Creates a new RunKickstandToPosition. */
+    public AcitvateKickstand(Kickstand kickstand) {
+        m_kickstand = kickstand;
+        // Use addRequirements() here to declare subsystem dependencies.
+        addRequirements(kickstand);
+    }
+
+    // Called when the command is initially scheduled.
+    @Override
+    public void initialize() {
+        m_kickstand.activateKickstand();
+    }
+
+    // Returns true when the command should end.
+    @Override
+    public boolean isFinished() {
+        return true;
+    }
+}
